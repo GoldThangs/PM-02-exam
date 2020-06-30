1e35fa9c126408851e51323319d8d90c0711f0ac
Tselikov Bogdan <bodyy.bog@gmail.com>
2020-06-30 09:04:36 +0500

    Проект в процессе разработки

--- unit1.pas
+++ unit1.pas
@@ -5,10 +5,36 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
 
 type
+
+  { TForm1 }
+
   TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Edit5: TEdit;
+    Edit6: TEdit;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
 
   public
@@ -19,8 +45,20 @@ var
   Form1: TForm1;
 
 implementation
-
+uses Unit2;
 {$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+Unit2.Form2.ShowModal;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+Close;
+end;
 
 end.
 
